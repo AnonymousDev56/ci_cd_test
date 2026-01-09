dc84543736dd7c8bc9b031de0866b66112331493
AnonymousDev56 <username1272120@gmail.com>
2026-01-09 18:09:50 +0500

    Fix indentation in test_root_endpoint

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
+import sys
+
+sys.path.append("/app")
+
 from app import app
 
+
 def test_root_endpoint():
-  client = app.test_client()
-  response = client.get("/")
-  assert response.status_code == 200
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
