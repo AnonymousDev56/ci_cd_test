08157ef0de0ab2346e8499af77819600928e38d2
AnonymousDev56 <username1272120@gmail.com>
2026-01-11 17:01:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return "Hello, CI/CD world!"
 
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
